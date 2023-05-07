0d6c65987bc20df92938720efced39293124ab8c
cyanpencil <lucadb96@gmail.com>
2023-05-07 17:45:47 +0200

    added example 3

--- run_docker.sh
+++ run_docker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker run -p 8080:80 --cap-add sys_ptrace --rm -v $(pwd)/website:/usr/share/nginx/html -it vizze
+sudo docker run -p 9999:80 --cap-add sys_ptrace --rm -v $(pwd)/website:/usr/share/nginx/html -it vizzed
