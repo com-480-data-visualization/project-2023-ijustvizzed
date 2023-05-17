eb81fada96419ae82db3428c65cdc3efd63b25ac
philippmao <test>
2023-05-17 19:16:52 +0200

    innerthtml

--- website/assets/js/descbox.js
+++ website/assets/js/descbox.js
@@ -8,7 +8,7 @@ console.log(elements);
 TODO: Update this and use the actual dataset
 */
 var consp_desc = {
-    "1" : "AFAFAFAFAFAFAF",
+    "0" : "<p>The <em>Chemtrails</em> conspiracy theory suggests that the condensation trails left behind by aircraft crossing the sky contain chemical and/or biological agents. Depending on the different nuances of the conspiracy theory, those agents can serve different purposes such as</p>\n<ul>\n<li>weather control,</li>\n<li>human population control, and</li>\n<li>chemical/biological warfare.</li>\n</ul>\n<p>More information: <a href=\"https://en.wikipedia.org/wiki/Chemtrail_conspiracy_theory\" class=\"uri\">https://en.wikipedia.org/wiki/Chemtrail_conspiracy_theory</a></p>",
     "2" : "TEEESTESTSTEETS"
 }
 
@@ -31,12 +31,12 @@ function handleMouseOver(event) {
         console.log(event.target.attributes);
         console.log(consp_id);
         console.log(consp_desc[consp_id]);
-        descbox.innerText = consp_desc[consp_id];
+        descbox.innerHTML = consp_desc[consp_id];
     }
 }
 function handleMouseOut(event) {
     if(!fixed){
-        descbox.innerText = "";
+        descbox.innerHTML = "";
     }
 }
 function handleClick(event) {
@@ -45,7 +45,7 @@ function handleClick(event) {
     console.log(event.target.attributes);
     console.log(consp_id);
     console.log(consp_desc[consp_id]);
-    descbox.innerText = consp_desc[consp_id];
+    descbox.innerHTML = consp_desc[consp_id];
 }
 
 
