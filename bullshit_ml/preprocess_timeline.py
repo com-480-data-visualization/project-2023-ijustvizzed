058f03e3435cb11936df43c0c6d0d07da060aa4f
cyanpencil <lucadb96@gmail.com>
2023-06-04 14:26:27 +0200

    cool labels

--- bullshit_ml/preprocess_timeline.py
+++ bullshit_ml/preprocess_timeline.py
@@ -8,14 +8,14 @@ from datetime import datetime
 
 print(THEORIES)
 
-publications = ["Axios", "Business Insider", "Buzzfeed News", "CNBC", "CNN", "Economist", "Fox News", "Gizmodo", "Hyperallergic", "Mashable", "New Republic", "New Yorker", "People", "Politico", "Refinery 29", "Reuters", "TMZ", "TechCrunch", "The Hill", "The New York Times", "The Verge", "Vice", "Vice News", "Vox", "Washington Post", "Wired"]
+publications = ["infowars", "Axios", "Business Insider", "Buzzfeed News", "CNBC", "CNN", "Economist", "Fox News", "Gizmodo", "Hyperallergic", "Mashable", "New Republic", "New Yorker", "People", "Politico", "Refinery 29", "Reuters", "TMZ", "TechCrunch", "The Hill", "The New York Times", "The Verge", "Vice", "Vice News", "Vox", "Washington Post", "Wired"]
 publications = list(map(lambda x: x.lower(), publications))
 
 data_pub = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: 0)))
 
 data_jones = defaultdict(lambda: defaultdict(lambda: 0))
 
-normalizations = defaultdict(int)
+normalizations = defaultdict(lambda: 1)
 
 
 lines = open("../news/preprocessed.csv", "r", errors='ignore')
@@ -44,6 +44,21 @@ for line in lines:
         pass
 
 
+for f in sorted(os.listdir("../infowars-main")):
+    print(f)
+    date = f.split("_")[0]
+    year, month, day = date[0:4], int(date[4:6]), date[6:]
+    if int(year) < 2016: continue
+    if int(year) >= 2021: continue
+    if int(year) == 2020 and month > 4: continue
+    date_string = f"{year}-{month}"
+
+    for theory in THEORIES:
+        the = " ".join(theory)
+        with open("../infowars-main/" + f, "r") as file:
+            count = file.read().count(the)
+        data_pub[the][date_string]["infowars"] += count
+        normalizations["infowars"] += 1
 
 
 for x in THEORIES:
@@ -69,40 +84,21 @@ for x in THEORIES:
                     f.write(",")
             f.write("\n")
 
-        # for e,pub in enumerate(publications):
-            # for date in data_pub[pub].keys():
-                # f.write(date+",")
-                # f.write(str(data_pub[pub][date][" ".join(x)]))
-            # if e != len(publications)-1:
+
+
+
+# with open("../website/assets/data/timeline_pub_infowars.csv", "w") as f:
+    # f.write("date,")
+    # for e,x in enumerate(THEORIES):
+        # f.write(" ".join(x))
+        # if e != len(THEORIES)-1:
+            # f.write(",")
+    # f.write("\n")
+    # for date in data_jones.keys():
+        # f.write(date+",")
+        # for e,x in enumerate(THEORIES):
+            # f.write(str(data_jones[date][" ".join(x)]))
+            # if e != len(THEORIES)-1:
                 # f.write(",")
-exit(1)
-
-
-for f in sorted(os.listdir("../infowars-main")):
-    print(f)
-    date = f.split("_")[0]
-    year, month, day = date[0:4], date[4:6], date[6:]
-    date_string = f"{year}-{month}"
-
-    for theory in THEORIES:
-        the = " ".join(theory)
-        with open("../infowars-main/" + f, "r") as file:
-            count = file.read().count(the)
-        data_jones[date_string][the] += count
-
-
-with open("../website/assets/data/timeline_jones.csv", "w") as f:
-    f.write("date,")
-    for e,x in enumerate(THEORIES):
-        f.write(" ".join(x))
-        if e != len(THEORIES)-1:
-            f.write(",")
-    f.write("\n")
-    for date in data_jones.keys():
-        f.write(date+",")
-        for e,x in enumerate(THEORIES):
-            f.write(str(data_jones[date][" ".join(x)]))
-            if e != len(THEORIES)-1:
-                f.write(",")
-        f.write("\n")
+        # f.write("\n")
 
